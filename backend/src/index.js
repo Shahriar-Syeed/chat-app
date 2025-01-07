12f6777f95f5bd4867fd188d7f4a2352c782bb80
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-01-07 18:42:34 +0600

    mongoose database created

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,18 @@
 // const express = require("express"); for js
 // for module
 import express from "express";
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./libs/db.js";
 
+dotenv.config();
 const app = express();
+
+const PORT = process.env.PORT ;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
-  console.log("Server is running on port 5001");
+app.listen(PORT, () => {
+  console.log("Server is running on port: " + PORT);
+  connectDB();
 });
