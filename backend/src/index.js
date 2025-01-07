7469dc57e1a5498c68b56178d9c69f28c4f9c28d
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-01-07 16:47:44 +0600

    index in src with package main and dev change to src, auth route js created with signup, login, logout express.Route();

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+// const express = require("express"); for js
+// for module
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+
+const app = express();
+
+app.use("/api/auth", authRoutes);
+
+app.listen(5001, () => {
+  console.log("Server is running on port 5001");
+});
