f0e5966fe0026c92ca26adc01af095d2079042d3
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-01-08 18:08:03 +0600

    protect route to check token middleware , profile pic update with cloudinary

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@
 // for module
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./libs/db.js";
 
@@ -11,6 +12,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
