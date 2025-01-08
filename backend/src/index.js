1d9bd98dd84c4737c22142edecba782ed0cdfe44
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-01-08 19:29:28 +0600

    start message route with message controller, model and route

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./libs/db.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const app = express();
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port: " + PORT);
