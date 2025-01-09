dfbde1963e869936f0b21f89a289205cd189cd07
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-01-09 19:22:26 +0600

    pages file created with root layout

---
+++ frontend/src/pages/RootLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar"
+
+function RootLayout() {
+  return (
+    <>
+    <Navbar>jogjsao;j</Navbar>
+    <Outlet/>
+    </>
+  )
+}
+
+export default RootLayout;
---
+++ frontend/src/routers/router.jsx
@@ -0,0 +1,40 @@
+import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "../pages/RootLayout.jsx";
+import HomePage from "../pages/HomePage.jsx";
+import SignUpPage from "../pages/SignUpPage.jsx";
+import LoginPage from "../pages/LoginPage.jsx";
+import SettingsPage from "../pages/SettingsPage.jsx";
+import ProfilePage from "../pages/ProfilePage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <RootLayout/>,
+
+    children:[
+      {
+        index:true,
+        element: <HomePage/>
+      },
+      {
+        path:"signup",
+        element: <SignUpPage/>
+      },
+      {
+        path:"login",
+        element: <LoginPage/>
+      },
+      {
+        path:"logout",
+        element: <ProfilePage/>
+      },
+      {
+        path:"settings",
+        element: <SettingsPage/>
+      },
+    ]
+
+  }
+]);
+
+export default router;
